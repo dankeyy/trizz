48ee93deac65c62a4c7b3a0d8326fbbb699e2e76
dankeyy <dankeyy@protonmail.com>
2023-04-18 17:02:07 +0300

    fix redundant | on deep entries preceding shallow entries
    before this, stuff like
    ```
    ...
    │   └── dir
    │   │   └── file1
    ├── file2
    ...
    
    ```
    could've been printed (file1 line)

--- trizz.zig
+++ trizz.zig
@@ -28,10 +28,17 @@ fn cmp(context: void, a: std.fs.IterableDir.Entry, b: std.fs.IterableDir.Entry) 
     return a.name.len < b.name.len;
 }
 
-fn printEntry(entryName: []const u8, level: usize, last: bool, colour: []const u8, symlinkedTo: ?[]u8) !void {
-    for (0..level) |_| _ = try stdout.write("│   ");
+fn printEntry(entryName: []const u8, level: usize, inNLasts: usize, isLast: bool, colour: []const u8, symlinkedTo: ?[]u8) !void {
+    for (0..level) |i| {
+        if ((level - i) > inNLasts - @boolToInt(isLast)) {
+            _ = try stdout.write("│   ");
+        }
+        else {
+            _ = try stdout.write("    ");
+        }
+    }
 
-    _ = try stdout.write(if (last) "└── " else "├── ");
+    _ = try stdout.write(if (isLast) "└── " else "├── ");
     _ = try stdout.write(colour);
     _ = try stdout.write(entryName);
     _ = try stdout.write("\x1b[0m"); // reset escape code
@@ -43,11 +50,11 @@ fn printEntry(entryName: []const u8, level: usize, last: bool, colour: []const u
 }
 
 // prints an entry, adds it to corresponding count, returns a flag indicating if recursion is needed (true for directories)
-inline fn printAndCountEntry(entry: std.fs.IterableDir.Entry, filePathBuf: []u8, path: []u8, cap: usize, level: usize, last: bool, c: *Counts) !bool {
+inline fn printAndCountEntry(entry: std.fs.IterableDir.Entry, filePathBuf: []u8, path: []u8, cap: usize, level: usize, inNLasts: usize, isLast: bool, c: *Counts) !bool {
     switch (entry.kind) {
         .Directory => {
             c.dir_count += 1;
-            try printEntry(entry.name, level, last, "\x1b[1;34m", null); // bold blue
+            try printEntry(entry.name, level, inNLasts, isLast, "\x1b[1;34m", null); // bold blue
             return true;
         },
 
@@ -59,31 +66,31 @@ inline fn printAndCountEntry(entry: std.fs.IterableDir.Entry, filePathBuf: []u8,
                 "\x1b[1;32m" // bold green
             else
                 "";
-            try printEntry(entry.name, level, last, colour, null);
+            try printEntry(entry.name, level, inNLasts, isLast, colour, null);
             c.file_count += 1;
         },
         .SymLink => {
             const newFileBuf = try std.fmt.bufPrint(filePathBuf.ptr[0..cap], "{s}/{s}", .{ path, entry.name });
             const linked = try std.os.readlink(newFileBuf, filePathBuf);
-            try printEntry(entry.name, level, last, "\x1b[1;35m", linked); // bold purple-ish
+            try printEntry(entry.name, level, inNLasts, isLast, "\x1b[1;35m", linked); // bold purple-ish
             c.file_count += 1;
         },
         .BlockDevice, .CharacterDevice => {
-            try printEntry(entry.name, level, last, "\x1b[1;33m", null); // bold yellow
+            try printEntry(entry.name, level, inNLasts, isLast, "\x1b[1;33m", null); // bold yellow
             c.file_count += 1;
         },
         .NamedPipe => {
-            try printEntry(entry.name, level, last, "\x1b[38;5;214m", null); // regular gold-ish
+            try printEntry(entry.name, level, inNLasts, isLast, "\x1b[38;5;214m", null); // regular gold-ish
             c.file_count += 1;
         },
         .UnixDomainSocket => {
-            try printEntry(entry.name, level, last, "\x1b[38;5;208m", null); // regular orange
+            try printEntry(entry.name, level, inNLasts, isLast, "\x1b[38;5;208m", null); // regular orange
             c.file_count += 1;
         },
 
         else => {
             // who cares about whiteout/ doors/ eventports/ unknown anyway
-            try printEntry(entry.name, level, last, "\x1b[1;90m", null); // bold black
+            try printEntry(entry.name, level, inNLasts, isLast, "\x1b[1;90m", null); // bold black
             c.file_count += 1;
         },
     }
@@ -98,7 +105,7 @@ inline fn copyEntry(nameBuf: []u8, nextEntry: *const std.fs.IterableDir.Entry) s
     };
 }
 
-fn walkUnsorted(nameBuf: []u8, path: []u8, filePathBuf: []u8, cap: usize, level: usize) !Counts {
+fn walkUnsorted(nameBuf: []u8, path: []u8, filePathBuf: []u8, cap: usize, level: usize, inNLasts: usize) !Counts {
     var counts = Counts{ .dir_count = 0, .file_count = 0 };
 
     var dir = try std.fs.cwd().openIterableDir(path, .{});
@@ -117,10 +124,10 @@ fn walkUnsorted(nameBuf: []u8, path: []u8, filePathBuf: []u8, cap: usize, level:
 
     while (try it.next()) |nextEntry| {
         if (!std.mem.startsWith(u8, entry.name, ".")) {
-            var needsToRecurse = try printAndCountEntry(entry, filePathBuf, path, cap, level, false, &counts);
+            var needsToRecurse = try printAndCountEntry(entry, filePathBuf, path, cap, level, inNLasts, false, &counts);
             if (needsToRecurse) {
                 var slice = try std.fmt.bufPrint(path.ptr[0..cap], "{s}/{s}", .{ path, entry.name });
-                const res = try walkUnsorted(nameBuf, slice, filePathBuf, cap, level + 1);
+                const res = try walkUnsorted(nameBuf, slice, filePathBuf, cap, level + 1, inNLasts);
 
                 counts.dir_count += res.dir_count;
                 counts.file_count += res.file_count;
@@ -128,10 +135,10 @@ fn walkUnsorted(nameBuf: []u8, path: []u8, filePathBuf: []u8, cap: usize, level:
         }
         entry = copyEntry(nameBuf, &nextEntry);
     }
-    var needsToRecurse = try printAndCountEntry(entry, filePathBuf, path, cap, level, true, &counts);
+    var needsToRecurse = try printAndCountEntry(entry, filePathBuf, path, cap, level, inNLasts + 1, true, &counts);
     if (needsToRecurse) {
         var slice = try std.fmt.bufPrint(path.ptr[0..cap], "{s}/{s}", .{ path, entry.name });
-        const res = try walkUnsorted(nameBuf, slice, filePathBuf, cap, level + 1);
+        const res = try walkUnsorted(nameBuf, slice, filePathBuf, cap, level + 1, inNLasts + 1);
 
         counts.dir_count += res.dir_count;
         counts.file_count += res.file_count;
@@ -141,7 +148,7 @@ fn walkUnsorted(nameBuf: []u8, path: []u8, filePathBuf: []u8, cap: usize, level:
     return counts;
 }
 
-fn walkSorted(allocator: *const std.mem.Allocator, path: []u8, filePathBuf: []u8, cap: usize, level: usize) !Counts {
+fn walkSorted(allocator: *const std.mem.Allocator, path: []u8, filePathBuf: []u8, cap: usize, level: usize, inNLasts: usize) !Counts {
     var counts = Counts{ .dir_count = 0, .file_count = 0 };
 
     var entries = FileArrayList.init(allocator.*);
@@ -162,11 +169,11 @@ fn walkSorted(allocator: *const std.mem.Allocator, path: []u8, filePathBuf: []u8
     std.sort.sort(std.fs.IterableDir.Entry, entries.items, {}, cmp);
 
     for (1.., entries.items) |i, entry| {
-        const last = i == entries.items.len;
-        var needsToRecurse = try printAndCountEntry(entry, filePathBuf, path, cap, level, last, &counts);
+        const isLast = i == entries.items.len;
+        var needsToRecurse = try printAndCountEntry(entry, filePathBuf, path, cap, level, inNLasts + @boolToInt(isLast), isLast, &counts);
         if (needsToRecurse) {
             var slice = try std.fmt.bufPrint(path.ptr[0..cap], "{s}/{s}", .{ path, entry.name });
-            const res = try walkSorted(allocator, slice, filePathBuf, cap, level + 1);
+            const res = try walkSorted(allocator, slice, filePathBuf, cap, level + 1, inNLasts + @boolToInt(isLast));
 
             counts.dir_count += res.dir_count;
             counts.file_count += res.file_count;
@@ -189,8 +196,8 @@ pub fn main() !void {
     var filePathBuf: [4096]u8 = undefined;
     var nameBuf: [255]u8 = undefined;
 
-    // const res = try walkSorted(&allocator, initialPath, &filePathBuf, 4096, 0);
-    const res = try walkUnsorted(&nameBuf, initialPath, &filePathBuf, 4096, 0);
+    // const res = try walkSorted(&allocator, initialPath, &filePathBuf, 4096, 0, 0);
+    const res = try walkUnsorted(&nameBuf, initialPath, &filePathBuf, 4096, 0, 0);
 
     const dirs = res.dir_count;
     const files = res.file_count;
